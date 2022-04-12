2f787097a5b4a1a42f85d31282fc359c6e92705a
Ivan Vergés <ivan@platoniq.net>
2022-04-12 21:22:22 +0200

    add sidekiq cron

--- config/environment/production.rb
+++ config/environment/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   config.log_tags = [ :request_id ]
@@ -62,6 +62,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "decidim-app_#{Rails.env}"
+
+  config.active_job.queue_adapter     = :sidekiq
 
   config.action_mailer.perform_caching = false
 
