bb747fca8f1764cab8768e172789dda70de90adb
Ivan Vergés <ivan@platoniq.net>
2022-05-20 13:46:11 +0200

    add action authorizar for 16-29 age span

---
+++ app/services/census_action_authorizer.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class CensusActionAuthorizer < Decidim::Verifications::DefaultActionAuthorizer
+  def authorize
+    return [:missing, { action: :authorize }] if authorization.blank?
+
+    return [:unauthorized, {}] if date_of_birth.blank?
+
+    @fields = { date_of_birth: date_of_birth }
+    return [:ok, {}] if age_allowed?
+
+    [:unauthorized, { fields: @fields }]
+  end
+
+  private
+
+  def age_allowed?
+    age = Time.zone.today.year - Date.parse(date_of_birth).year
+    return if age < 16
+
+    age < 30
+  rescue StandardError => e
+    Rails.logger.error "ACTION AUTHORIZER ERROR: #{e.message}"
+  end
+
+  def date_of_birth
+    authorization.metadata["date_of_birth"]
+  end
+
+  def manifest
+    @manifest ||= Decidim::Verifications.find_workflow_manifest(authorization&.name)
+  end
+end
