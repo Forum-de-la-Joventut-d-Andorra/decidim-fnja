96c314f38fddc0444d9894ce0583a668c5635474
Ivan Vergés <ivan@pokecode.net>
2024-09-23 13:24:53 +0200

    remove initializer

--- config/initializers/verifications_and_overrides.rb
+++ config/initializers/verifications_and_overrides.rb
@@ -1,23 +1,17 @@
 # frozen_string_literal: true
 
-# We are using the same DirectVerifications engine without the admin part to
-# create a custom verification method called "direct_verifications_managers"
-# Decidim::Verifications.register_workflow(:direct_verifications_managers) do |workflow|
-#   workflow.engine = Decidim::DirectVerifications::Verification::Engine
-# end
-
 Decidim::Verifications.register_workflow(:census_authorization_handler) do |workflow|
   workflow.form = "CensusAuthorizationHandler"
   workflow.action_authorizer = "YouthCensusActionAuthorizer"
 end
 
 # We need to tell the plugin to handle this method in addition to the default "Direct verification". Any registered workflow is valid.
-Decidim::DirectVerifications.configure do |config|
-  config.manage_workflows = %w(direct_verifications id_documents)
+# Decidim::DirectVerifications.configure do |config|
+#   config.manage_workflows = %w(direct_verifications id_documents)
 
-  # change the to the metadata_parser if you want it
-  # config.input_parser = :metadata_parser
-end
+#   # change the to the metadata_parser if you want it
+#   # config.input_parser = :metadata_parser
+# end
 # Remove NIE from id_documents form
 Decidim::Verifications.configure do |config|
   config.document_types = %w(DNI passport)
